76d9c8d477b91530acf503f262e3f7acf8973458
maitri <jainmaitri120@gmail.com>
2022-02-03 12:46:29 +0530

    postgresql edit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+#import re
 
 from flask import Flask
 from flask_restful import Api
@@ -10,9 +11,12 @@ from resources.item import Item, ItemList
 from resources.store import Store,StoreList
 from db import db
 
+#uri=os.getenv("DATABASE_URL")
+#if uri.startswith("postgres://"):
+#    uri=uri.replace("postgres://","postgresql://",1)
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL','sqlite:///data.db')
+app.con2fig['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 app.secret_key= 'maitri'
 api= Api(app)
@@ -25,7 +29,7 @@ api.add_resource(StoreList,'/stores')
 api.add_resource(ItemList,'/items')
 api.add_resource(UserRegister,'/register')
 
+db.init_app(app)
 if __name__=='__main__':
-    db.init_app(app)
     app.run(port=5000,debug= True)
     
